b61f9415b9f9770e1d8bc551e209c18617ee6fc6
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-31 11:00:24 -0800

    Add header

--- index.php
+++ index.php
@@ -1,30 +1,12 @@
 <?php
 
-// caps indicates constant
-define('NAME', 'Deja');
-
-// $name = 'Deja';
-$age = 29;
-
-// $name = 'Natalie';
-
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
+  <?php include('templates/header.php'); ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My First PHP File</title>
-</head>
-
-<body>
-  <h1><?php echo 'Helloooo'; ?></h1>
-
-  <div><?php echo NAME; ?></div>
-  <div><?php echo $age; ?></div>
 </body>
 
 </html>
---
+++ templates/header.php
@@ -0,0 +1,29 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rathaus Pizza</title>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <style type="text/css">
+    .brand {
+      background: #cbb09c;
+    }
+    .brand-text {
+      color: #cbb09c !important;
+    }
+  </style>
+</head>
+  <!-- Using Materialize helper classes -->
+  <body class="grey lighten-4">
+  <!-- By default, Materialize gives nav dropshadow; z-depth-0 removes it -->
+  <nav class="white z-depth-0">
+    <!-- container class keeps material in central column rather than full width across page -->
+    <div class="container">
+      <!-- brand-text is not a Materialize class, but a custom one -->
+      <a href="#" class="brand-logo brand-text">Rathaus Pizza</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <!-- brand is a custom class -->
+        <li><a href="#" class="btn brand z-depth-0">Add a pizza</a></li>
+      </ul>
+    </div>
+  </nav>
