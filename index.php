571139709fce03aef718500c8732f65ec1c0ad04
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-31 17:33:53 -0800

    Connect to database

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
+// Can connect to database with MySQLi (i: improved) or PDO (PHP Data Objects)
+// Connect to database
+$conn = mysqli_connect('localhost', 'nc', '4HxCaiJJK5Ja1oQq', 'rathaus_pizza');
+
+// Check connection
+if (!$conn) {
+  echo 'Connection error: ' . mysqli_connect_error();
+}
+
 ?>
 
 
