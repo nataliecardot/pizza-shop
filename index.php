0ad88a8150b19bb2beb889a1c4273d09a735f2e6
Natalie-Cardot <nataliecardot@gmail.com>
2021-02-01 19:14:13 -0800

    Enable getting single record

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ mysqli_close($conn);
               </ul>
             </div>
             <div class="card-action right-align">
-              <a href="#" class="brand-text">More info</a>
+              <a href="details.php?id=<?php echo $pizza['id']; ?>" class="brand-text">More info</a>
             </div>
           </div>
         </div>
