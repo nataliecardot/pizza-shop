7207b706a68bbefaddc0fe01b3b14d270c559fe2
Natalie-Cardot <nataliecardot@gmail.com>
2021-02-01 20:26:13 -0800

    Enable deletion of record

--- details.php
+++ details.php
@@ -2,6 +2,21 @@
 
   include('config/db_connect.php');
 
+  // Get value from POST array available after delete button is clicked (value is filled with id from GET request submitted when more info button is clicked)
+  if (isset($_POST['delete'])) {
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if (mysqli_query($conn, $sql)) {
+      // Success
+      header('Location: index.php');
+    } else {
+      // Failure
+      echo 'Query error: ' . mysqli_error();
+    }
+  }
+
   // Check GET request id parameter
   if (isset($_GET['id'])) {
     $id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -34,6 +49,12 @@
     <p><?php echo date($pizza['created_at']); ?></p>
     <h5>Ingredients:</h5>
     <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+    <!-- DELETE FORM -->
+    <form action="details.php" method="POST">
+      <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+      <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+    </form>
 
   <?php else: ?>
     <h5>No such pizza exists</h5>
